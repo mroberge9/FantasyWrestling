1baae2c379573f8ea4c589bd36903ad78c738608
Michael Roberge <mroberge@umich.edu>
2023-10-17 17:12:12 -0400

    sql

---
+++ sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO wrestlers(name, wins, losses, isWorldChamp, isTVChamp, numWorldTitles, numTVTitles)
+VALUES
+('MJF', 0, 0, 0, 0, 0, 0),
+('Bryan Danielson', 0, 0, 0, 0, 0, 0);
---
+++ sql/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE wrestlers(
+    name VARCHAR(40) NOT NULL,
+    wins INTEGER NOT NULL,
+    losses INTEGER NOT NULL,
+    isWorldChamp BOOL NOT NULL,
+    isTVChamp BOOL NOT NULL,
+    numWorldTitles INTEGER NOT NULL,
+    numTVTitles INTEGER NOT NULL
+);
